1f85dc716e0fcf0a450b3d7ee5ccde553f7c471f
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-12 12:36:48 -0400

    Initial request implementation

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,25 @@
 const request = require("request");
 
+const requestKitty = function(url, callback) {
+
+  request(url, (error, response, body) => {
+    if (!error) {
+      if (response.statusCode === 200) {
+        callback(body);
+      } else {
+        console.log("request.statusCode:", response && response.statusCode);
+      }
+    } else {
+      console.log("request error:", error);
+    }
+  });
+
+};
+
+requestKitty("https://api.thecatapi.com/v1/breeds/search?q=sib", (body) => {
+
+  console.log(typeof body, body);
+  let kitty = JSON.parse(body);
+  console.log(typeof kitty, kitty);
+
+});
